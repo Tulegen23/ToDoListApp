bcaafd509ab09cfddeb8c40aa06b16584e617328
Erooxa <yernar.daldabay@narxoz.kz>
2025-06-01 19:04:26 +0500

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by almaty on 01.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by almaty on 01.06.2025.
+//
+
+import Testing
+@testable import ToDoListApp
+
+struct ToDoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
